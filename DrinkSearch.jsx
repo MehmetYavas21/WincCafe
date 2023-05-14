9f156bb45e420b44cb5084bcc624386c86d60b5b
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-14 11:09:12 +0200

    Update DrinkSearch.jsx

--- DrinkSearch.jsx
+++ DrinkSearch.jsx
@@ -6,13 +6,17 @@ import { TextInput } from './ui/TextInput';
 export const DrinkSearch = ({ clickFn }) => {
   const [searchField, setSearchField] = useState('test drink');
   const handleChange = e => setSearchField(e.target.value);
+  const matchedDrinks = availableDrinks.filter((drink) => {
+    return drink.name.toLowerCase().includes(searchField.toLowerCase());
+});
+  searchField="";
   
 
   return (
     <>
       <label>Search for drinks:</label>
       <TextInput changeFn={handleChange}/>
-      <DrinkList clickFn={clickFn} drinks={availableDrinks} />
+      <DrinkList clickFn={clickFn} drinks={matchedDrinks} />
     </>
   );
 };
