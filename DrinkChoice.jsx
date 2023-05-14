d3f07061903dd5c85ead013675fa408f6313d679
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-14 11:19:24 +0200

    Update DrinkChoice.jsx

--- DrinkChoice.jsx
+++ DrinkChoice.jsx
@@ -8,7 +8,7 @@ export const DrinkChoice = ({ drink , clickFn}) => {
 			<h2>Your choice: {drink.name}</h2>
 			<img src={drink.imgUrl} width={100} height={100} alt={drink.alt} />
 			<p>Your drink will be ready in a few minutes</p>
-			<Button text={changeDrink}  clickFn={() => clickFn()}/>
+			<Button text={changeDrink}  onClick={() => onClick()} />
 		</>
 	);
 };
