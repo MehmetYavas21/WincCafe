6632c7e45d4fd810225b8da3ae3b533c3ab03eda
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-14 11:21:43 +0200

    Update DrinkChoice.jsx

--- DrinkChoice.jsx
+++ DrinkChoice.jsx
@@ -1,14 +1,14 @@
 
 import {Button} from './ui/Button';;
 
-export const DrinkChoice = ({ drink , clickFn}) => {
+export const DrinkChoice = ({ drink , onClick}) => {
 	const changeDrink = 'Change';
 	return (
 		<>
 			<h2>Your choice: {drink.name}</h2>
 			<img src={drink.imgUrl} width={100} height={100} alt={drink.alt} />
 			<p>Your drink will be ready in a few minutes</p>
-			<Button text={changeDrink}  clickFn={() => clickFn()} />
+			<Button text={changeDrink}  onClick={() => onClick()} />
 		</>
 	);
 };
