f0992cf9fcb2ecbfd9c97ff5a6124e7759e7389f
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-16 18:32:29 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 
+import { Center, Heading } from '@chakra-ui/react'
 import { useState } from 'react';
 import './App.css';
 import { DrinkChoice } from './components/DrinkChoice';
 import { DrinkSearch } from './components/DrinkSearch';
-import { Heading } from '@chakra-ui/react'
 
 
 export const App = () => {
@@ -14,7 +14,7 @@ export const App = () => {
   return (
     <div className="app">
       {userDrink ? (
-        <DrinkChoice drink={userDrink} />
+        <DrinkChoice drink={userDrink} clickFn={setUserDrink} />
       ) : (
         <>
           <Heading mb={8} fontSize="2xl" color="blue.400">
