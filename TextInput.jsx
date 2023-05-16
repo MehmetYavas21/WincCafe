fa0ff671c4f789ff369beffafbdc89e3288f2a18
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-05-16 13:06:05 +0200

    Update TextInput.jsx
    styling with Chakra

--- TextInput.jsx
+++ TextInput.jsx
@@ -1,5 +1,5 @@
 import './TextInput.css';
+import { Input } from '@chakra-ui/react';
 
-export const TextInput = ({ changeFn }) => {
-    return <input className="input" onChange={changeFn}></input>;
-};
+
+export const TextInput = ({ changeFn }) => ( <Input variant='filled' placeholder='Filled' onChange={changeFn} />);
